10658510f92fc065d6a0b4460de067d2ed29fe39
Andrey Nazarenko <limelaudio@gmail.com>
2024-02-23 08:54:06 +0200

    add message

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import { useTranslations } from 'next-intl'
 import { getTranslations } from 'next-intl/server'
-
+import Image from 'next/image'
 export async function generateMetadata({
   params: { locale },
 }: {
@@ -14,13 +14,26 @@ export async function generateMetadata({
 }
 
 export default function Home() {
-  const t = useTranslations()
+  const t = useTranslations('workInProgress')
+  // console.log(message)
   return (
-    <main className=''>
-      <div className='container'>
-        <span>test</span>
-        <h1>{t('Metadata.title')}</h1>
-        <p>description</p>
+    <main className='min-h-[100vh]'>
+      <div className='container text-center flex items-center justify-center flex-col'>
+        <p className='text-h0 text-accent'>{t('title')}</p>
+        <br />
+        <p className='text-h2'>{t('message')}</p>
+        <br />
+        <p className='text-h2'>{t('message2')}</p>
+        <br />
+        <p className='text-h2'>{t('message3')}</p>
+
+        <Image
+          className='w-[300px] h-[300px]'
+          src='/Thought-Processes.svg'
+          alt='Thougth Procces'
+          width={865}
+          height={682}
+        />
       </div>
     </main>
   )
--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,48 +1,48 @@
 'use client'
-import { useEffect, useRef, useState } from 'react'
-import debounce from 'debounce'
+import { useEffect, useRef } from 'react'
+// import debounce from 'debounce'
 // import clsx from 'clsx'
 import Link from 'next/link'
-import { HeaderActions as Actions } from './HeaderActions'
+// import { HeaderActions as Actions } from './HeaderActions'
 import { HeaderLangSwitcher as LangSwitcher } from './HeaderLangSwitcher'
-import { HeaderCategories as Categories } from './HeaderCategories'
+// import { HeaderCategories as Categories } from './HeaderCategories'
 
 function Header() {
   const headerRef = useRef<HTMLElement>(null)
-  const [prevScroll, setPrevScroll] = useState(200)
+  // const [prevScroll, setPrevScroll] = useState(200)
 
-  function updateFixedState() {
-    const currentScroll = window.scrollY
-    const isScrollingUp = currentScroll < prevScroll
-    if (headerRef.current) {
-      currentScroll > 0
-        ? headerRef.current.classList.add('header_fixed')
-        : headerRef.current.classList.remove('header_fixed')
+  // function updateFixedState() {
+  //   const currentScroll = window.scrollY
+  //   const isScrollingUp = currentScroll < prevScroll
+  //   if (headerRef.current) {
+  //     currentScroll > 0
+  //       ? headerRef.current.classList.add('header_fixed')
+  //       : headerRef.current.classList.remove('header_fixed')
 
-      if (window.innerWidth > 1024) {
-        if (currentScroll < 800) {
-          headerRef.current.classList.remove('header_hide')
-        } else {
-          isScrollingUp
-            ? headerRef.current.classList.remove('header_hide')
-            : headerRef.current.classList.add('header_hide')
-        }
-      } else {
-        headerRef.current.classList.remove('header_hide')
-      }
-    }
-    setPrevScroll(currentScroll)
-  }
+  //     if (window.innerWidth > 1024) {
+  //       if (currentScroll < 800) {
+  //         headerRef.current.classList.remove('header_hide')
+  //       } else {
+  //         isScrollingUp
+  //           ? headerRef.current.classList.remove('header_hide')
+  //           : headerRef.current.classList.add('header_hide')
+  //       }
+  //     } else {
+  //       headerRef.current.classList.remove('header_hide')
+  //     }
+  //   }
+  //   setPrevScroll(currentScroll)
+  // }
 
-  const debounceHandler = debounce(updateFixedState, 100)
+  // const debounceHandler = debounce(updateFixedState, 100)
 
   useEffect(() => {
     if (headerRef.current) {
       const headerHeight = headerRef.current.offsetHeight
       document.body.style.paddingTop = `${headerHeight}px`
     }
-    window.addEventListener('scroll', debounceHandler)
-    return () => window.removeEventListener('scroll', debounceHandler)
+    // window.addEventListener('scroll', debounceHandler)
+    // return () => window.removeEventListener('scroll', debounceHandler)
   })
 
   return (
@@ -60,25 +60,9 @@ function Header() {
                 <use href='/sprite.svg#logo' />
               </svg>
             </Link>
-            <Actions />
-          </nav>
-        </div>
-
-        <div className='header-bottom'>
-          <nav className='container flex items-center justify-between'>
-            <Link href='/' className='logo'>
-              <svg>
-                <use href='/sprite.svg#logo' />
-              </svg>
-            </Link>
-            <Categories />
-            <Actions />
           </nav>
         </div>
       </header>
-      <div className='header-info'>
-        <span>header info</span>
-      </div>
     </>
   )
 }
