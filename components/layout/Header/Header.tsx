25b72c2dc3972794a0fcdec4f826d019dd372ef3
Andrey Nazarenko <limelaudio@gmail.com>
2024-02-23 08:25:54 +0200

    fix changes before checkout

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -5,6 +5,7 @@ import debounce from 'debounce'
 import Link from 'next/link'
 import { HeaderActions as Actions } from './HeaderActions'
 import { HeaderLangSwitcher as LangSwitcher } from './HeaderLangSwitcher'
+import { HeaderCategories as Categories } from './HeaderCategories'
 
 function Header() {
   const headerRef = useRef<HTMLElement>(null)
@@ -70,30 +71,7 @@ function Header() {
                 <use href='/sprite.svg#logo' />
               </svg>
             </Link>
-            <menu className='flex items-center justify-start gap-10'>
-              <li>
-                <Link
-                  href='/'
-                  className='flex items-center justify-start text-h6 uppercase'
-                >
-                  Одежда
-                  <svg className='w-5 h-5 text-blue'>
-                    <use href='/sprite.svg#chevron-down' />
-                  </svg>
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href='/'
-                  className='flex items-center justify-start text-h6 uppercase'
-                >
-                  Обувь
-                  <svg className='w-5 h-5 text-blue'>
-                    <use href='/sprite.svg#chevron-down' />
-                  </svg>
-                </Link>
-              </li>
-            </menu>
+            <Categories />
             <Actions />
           </nav>
         </div>
