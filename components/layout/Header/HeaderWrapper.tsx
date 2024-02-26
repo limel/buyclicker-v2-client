5eb62ec794a4ca56f02c2a5c14335adbb04df9ee
Andrey Nazarenko <limelaudio@gmail.com>
2024-02-26 23:30:44 +0200

    fix header throttle function

--- components/layout/Header/HeaderWrapper.tsx
+++ components/layout/Header/HeaderWrapper.tsx
@@ -1,51 +1,67 @@
 'use client'
 import React from 'react'
 import { useEffect, useRef, useState, useLayoutEffect } from 'react'
-import debounce from 'debounce'
+import throttle from 'throttleit'
+import { HeaderContext } from 'context/HeaderContext'
 
 export function HeaderWrapper({ children }: { children: React.ReactNode }) {
   const headerRef = useRef<HTMLElement>(null)
-  const [prevScroll, setPrevScroll] = useState(200)
-
-  function updateFixedState() {
-    const currentScroll = window.scrollY
-    const isScrollingUp = currentScroll < (prevScroll || 0)
-
-    if (headerRef.current) {
-      currentScroll > 0
-        ? headerRef.current.classList.add('header_fixed')
-        : headerRef.current.classList.remove('header_fixed')
-
-      if (window.innerWidth > 1024) {
-        if (currentScroll < 800) {
-          headerRef.current.classList.remove('header_hide')
-        } else {
-          isScrollingUp
-            ? headerRef.current.classList.remove('header_hide')
-            : headerRef.current.classList.add('header_hide')
-        }
-      } else {
-        headerRef.current.classList.remove('header_hide')
-      }
-    }
-    setPrevScroll(currentScroll)
-  }
-
-  const debounceHandler = debounce(updateFixedState, 30)
+  const [activeIndex, setActiveIndex] = useState<number | null>(null)
   useLayoutEffect(() => {
     if (headerRef.current) {
       const headerHeight = headerRef.current.offsetHeight
       document.body.style.paddingTop = `${headerHeight}px`
     }
   }, [])
+
   useEffect(() => {
-    window.addEventListener('scroll', debounceHandler)
-    return () => window.removeEventListener('scroll', debounceHandler)
-  }, [debounceHandler, prevScroll])
+    let prevScroll = 200
+
+    function updateFixedState() {
+      const currentScroll = window.scrollY
+      const isScrollingUp = currentScroll < (prevScroll || 0)
+
+      function headerHide() {
+        setActiveIndex(null)
+        headerRef.current && headerRef.current.classList.add('header_hide')
+      }
+
+      if (headerRef.current) {
+        currentScroll > 0
+          ? headerRef.current.classList.add('header_fixed')
+          : headerRef.current.classList.remove('header_fixed')
+
+        if (window.innerWidth > 1024) {
+          if (currentScroll < 800) {
+            headerRef.current.classList.remove('header_hide')
+          } else {
+            isScrollingUp
+              ? headerRef.current.classList.remove('header_hide')
+              : headerHide()
+          }
+        } else {
+          headerRef.current.classList.remove('header_hide')
+        }
+      }
+      prevScroll = currentScroll
+    }
+
+    const throttleHandler = throttle(updateFixedState, 300)
+
+    window.addEventListener('scroll', throttleHandler)
+    return () => window.removeEventListener('scroll', throttleHandler)
+  }, [])
 
   return (
     <header className='header' ref={headerRef}>
-      {children}
+      <HeaderContext.Provider
+        value={{
+          activeIndex,
+          setActiveIndex,
+        }}
+      >
+        {children}
+      </HeaderContext.Provider>
     </header>
   )
 }
